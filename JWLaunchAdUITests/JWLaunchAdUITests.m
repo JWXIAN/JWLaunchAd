467702aa7d62136f81a05224105404e83b21bbeb
GJW <320891162@qq.com>
2016-07-25 11:26:30 +0800

    1.1 新增GIF图片自动加载

--- JWLaunchAdUITests/JWLaunchAdUITests.m
+++ JWLaunchAdUITests/JWLaunchAdUITests.m
@@ -21,6 +21,7 @@
     
     // In UI tests it is usually best to stop immediately when a failure occurs.
     self.continueAfterFailure = NO;
+    
     // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
     [[[XCUIApplication alloc] init] launch];
     
