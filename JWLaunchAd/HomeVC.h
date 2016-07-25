a8034ec7f719c55b1aeb6b3d8fa53903a9c5251c
GJW <320891162@qq.com>
2016-07-25 11:37:01 +0800

    1.1 添加GIF图片自动加载

---
+++ JWLaunchAd/HomeVC.h
@@ -0,0 +1,13 @@
+//
+//  HomeVC.h
+//  JWLaunchAd
+//
+//  Created by GJW on 16/7/25.
+//  Copyright © 2016年 JW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeVC : UIViewController
+
+@end
